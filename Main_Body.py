a9e207ba303e42be6fafbbeb0c10e9aba4105a8d
naz5617 <nazahorian@gmail.com>
2019-04-06 13:15:30 -0400

    fuck

--- Main_Body.py
+++ Main_Body.py
@@ -1,12 +1,35 @@
 from asterisk.agi import *
 
-def main():
 
-    agi = AGI()
-    agi.verbose("python agi started")
-    callerId = agi.env['agi callerid']
-    agi.verbose("Call from %s" %callerId)
 
-    agi.answer()
-    agi.stream_file('greeting_audio')
-    agi.hangup()
+
+agi = AGI()
+agi.verbose("python agi started")
+caller_id = agi.env['agi callerid']
+agi.verbose("Call from %s" % caller_id)
+agi.answer()
+agi.stream_file('greeting_audio')
+agi.hangup()
+
+
+
+
+
+
+
+
+
+"""
+myVar = 'something'
+Some variable
+
+agi = AGI()
+
+creates an agi instance
+
+
+agi.answer()
+
+I/O Communication: stdin, stdout, stderr
+
+"""
---
+++ help info.py
@@ -0,0 +1,16 @@
+import asterisk
+import asterisk.agi
+
+
+
+
+
+def main():
+    help(asterisk)
+    #help(asterisk.agi)
+    #help(asterisk.manager)
+    #help(asterisk.config)
+
+
+if __name__ == "__main__":
+    main()
