ae92f4420c8e25b5b70dbb6ff63665ed9a54dded
naz5617 <nazahorian@gmail.com>
2019-04-08 20:38:42 -0400

    started building out basic concept design

--- help me.py
+++ help me.py
@@ -13,12 +13,99 @@ if __name__ == '__main__':
         (dtmf_character, offset) = response
     num = agi.execute(pystrix.agi.core.GetVariable)
     agi.execute(pystrix.agi.core.ControlStreamFile('intro.wav'))
-    if num ==1:
-        agi.execute(pystrix.agi.core.ControlStreamFile('intro.wav'))
+    agi.execute(pystrix.agi.core.ControlStreamFile('max.wav'))
+    agi.execute(pystrix.agi.core.ControlStreamFile('joe.wav'))
+    agi.execute(pystrix.agi.core.ControlStreamFile('angela.wav'))
+    if num == 1:
+        agi.execute(pystrix.agi.core.ControlStreamFile('max.wav'))
+        num = agi.execute(pystrix.agi.core.GetVariable)
+        if num == 1:
+            num = agi.execute(pystrix.agi.core.GetVariable)
+            # transfer call to conference room
+        elif num == 2:
+            num = agi.execute(pystrix.agi.core.GetVariable)
+            agi.execute(pystrix.agi.core.ControlStreamFile('max.wav'))
+            # Start Live chat info
+            if num == 1:
+                agi.execute(pystrix.agi.core.ControlStreamFile('basic.wav'))
+            elif num == 2:
+                agi.execute(pystrix.agi.core.ControlStreamFile('basic+.wav'))
+            elif num == 3:
+                agi.execute(pystrix.agi.core.ControlStreamFile('brutal.wav'))
+            elif num == 4:
+                agi.execute(pystrix.agi.core.ControlStreamFile('brutal+.wav'))
+            elif num == 5:
+                agi.execute(pystrix.agi.core.ControlStreamFile('other.wav'))
+            elif num == 6:
+                agi.execute(pystrix.agi.core.ControlStreamFile('Ultra.wav'))
+            elif num == 7:
+                agi.execute(pystrix.agi.core.ControlStreamFile('Specials.wav'))
+
+        elif num == 3:
+            num = agi.execute(pystrix.agi.core.GetVariable)
+            # go into the pre-made porno listings random five.
+        else:
+            agi.execute(pystrix.agi.core.Hangup())
     elif num == 2:
-        return(num)
-    elif num ==3:
-        return (num)
+        agi.execute(pystrix.agi.core.ControlStreamFile('joe.wav'))
+        num = agi.execute(pystrix.agi.core.GetVariable)
+        if num == 1:
+            num = agi.execute(pystrix.agi.core.GetVariable)
+            # transfer call to conference room
+        elif num == 2:
+            num = agi.execute(pystrix.agi.core.GetVariable)
+            agi.execute(pystrix.agi.core.ControlStreamFile('joe.wav'))
+            if num == 1:
+                agi.execute(pystrix.agi.core.ControlStreamFile('basic.wav'))
+            elif num == 2:
+                agi.execute(pystrix.agi.core.ControlStreamFile('basic+.wav'))
+            elif num == 3:
+                agi.execute(pystrix.agi.core.ControlStreamFile('brutal.wav'))
+            elif num == 4:
+                agi.execute(pystrix.agi.core.ControlStreamFile('brutal+.wav'))
+            elif num == 5:
+                agi.execute(pystrix.agi.core.ControlStreamFile('other.wav'))
+            elif num == 6:
+                agi.execute(pystrix.agi.core.ControlStreamFile('Ultra.wav'))
+            elif num == 7:
+                agi.execute(pystrix.agi.core.ControlStreamFile('Specials.wav'))
+
+            # Start Live chat info
+        elif num == 3:
+            num = agi.execute(pystrix.agi.core.GetVariable)
+            # go into the pre-made porno listings random five.
+        else:
+            agi.execute(pystrix.agi.core.Hangup())
+    elif num == 3:
+        agi.execute(pystrix.agi.core.ControlStreamFile('angela.wav'))
+        num = agi.execute(pystrix.agi.core.GetVariable)
+        if num == 1:
+            num = agi.execute(pystrix.agi.core.GetVariable)
+            # transfer call to conference room
+        elif num == 2:
+            num = agi.execute(pystrix.agi.core.GetVariable)
+            agi.execute(pystrix.agi.core.ControlStreamFile('angela.wav'))
+            if num == 1:
+                agi.execute(pystrix.agi.core.ControlStreamFile('basic.wav'))
+            elif num == 2:
+                agi.execute(pystrix.agi.core.ControlStreamFile('basic+.wav'))
+            elif num == 3:
+                agi.execute(pystrix.agi.core.ControlStreamFile('brutal.wav'))
+            elif num == 4:
+                agi.execute(pystrix.agi.core.ControlStreamFile('brutal+.wav'))
+            elif num == 5:
+                agi.execute(pystrix.agi.core.ControlStreamFile('other.wav'))
+            elif num == 6:
+                agi.execute(pystrix.agi.core.ControlStreamFile('Ultra.wav'))
+            elif num == 7:
+                agi.execute(pystrix.agi.core.ControlStreamFile('Specials.wav'))
+
+            # Start Live chat info
+        elif num == 3:
+            num = agi.execute(pystrix.agi.core.GetVariable)
+            # go into the pre-made porno listings random five.
+        else:
+            agi.execute(pystrix.agi.core.Hangup())
     else:
         agi.execute(pystrix.agi.core.Hangup())
 
