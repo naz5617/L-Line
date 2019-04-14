9ed86664f11f9798b5a401eeaf507f727060c8ab
naz5617 <nazahorian@gmail.com>
2019-04-13 21:28:28 -0400

    Changed all the base audio names to the real ones

--- help me.py
+++ help me.py
@@ -1,5 +1,6 @@
 from pystrix import *
-
+import random
+import time
 
 if __name__ == '__main__':
     agi = pystrix.agi.AGI()
@@ -12,63 +13,115 @@ if __name__ == '__main__':
     agi.execute(pystrix.agi.core.ControlStreamFile('joe.wav'))
     agi.execute(pystrix.agi.core.ControlStreamFile('angela.wav'))
     if num == 1:
-        agi.execute(pystrix.agi.core.ControlStreamFile('max.wav'))
+        intro = random.randint(1, 4)
+        if intro == 1:
+            agi.execute(pystrix.agi.core.ControlStreamFile('Max_Intro_1'))
+        elif intro == 2:
+            agi.execute(pystrix.agi.core.ControlStreamFile('Max_Intro_2'))
+        elif intro == 3:
+            agi.execute(pystrix.agi.core.ControlStreamFile('Max_Intro_3'))
+        else:
+            agi.execute(pystrix.agi.core.ControlStreamFile('Max_Intro_4'))
+        time.sleep(2)
+        agi.execute(pystrix.agi.core.ControlStreamFile('Max_Introduction'))
+        time.sleep(2)
+        agi.execute(pystrix.agi.core.ControlStreamFile('Max_Intro_Response'))
         num = agi.execute(pystrix.agi.core.WaitForDigit)
         if num == 1:
             num = agi.execute(pystrix.agi.core.WaitForDigit)
             # transfer call to conference room
         elif num == 2:
             num = agi.execute(pystrix.agi.core.WaitForDigit)
-            agi.execute(pystrix.agi.core.ControlStreamFile('max.wav'))
+            agi.execute(pystrix.agi.core.ControlStreamFile('Max_Intro_LiveMenu'))
             # Start Live chat info
             if num == 1:
-                agi.execute(pystrix.agi.core.ControlStreamFile('basic.wav'))
+                agi.execute(pystrix.agi.core.ControlStreamFile('Max_Basic'))
             elif num == 2:
-                agi.execute(pystrix.agi.core.ControlStreamFile('basic+.wav'))
+                agi.execute(pystrix.agi.core.ControlStreamFile('Max_Basic+'))
             elif num == 3:
-                agi.execute(pystrix.agi.core.ControlStreamFile('brutal.wav'))
+                agi.execute(pystrix.agi.core.ControlStreamFile('Max_Brutal'))
             elif num == 4:
-                agi.execute(pystrix.agi.core.ControlStreamFile('brutal+.wav'))
+                agi.execute(pystrix.agi.core.ControlStreamFile('Max_Brutal+'))
             elif num == 5:
-                agi.execute(pystrix.agi.core.ControlStreamFile('other.wav'))
+                agi.execute(pystrix.agi.core.ControlStreamFile('Max_Other'))
             elif num == 6:
-                agi.execute(pystrix.agi.core.ControlStreamFile('Ultra.wav'))
+                agi.execute(pystrix.agi.core.ControlStreamFile('Max_Ultra'))
             elif num == 7:
-                agi.execute(pystrix.agi.core.ControlStreamFile('Specials.wav'))
+                agi.execute(pystrix.agi.core.ControlStreamFile('Max_Specials_Intro'))
+                spec = random.randint(1, 6)
+                if spec == 1:
+                    agi.execute(pystrix.agi.core.ControlStreamFile('Max_UnderTheSea'))
+                elif spec == 2:
+                    agi.execute(pystrix.agi.core.ControlStreamFile('Max_Lovecraft'))
+                elif spec == 3:
+                    agi.execute(pystrix.agi.core.ControlStreamFile('Max_Rainbow'))
+                elif spec == 4:
+                    agi.execute(pystrix.agi.core.ControlStreamFile('Max_Fruit'))
+                elif spec == 5:
+                    agi.execute(pystrix.agi.core.ControlStreamFile('Max_Bees'))
+                else:
+                    agi.execute(pystrix.agi.core.ControlStreamFile('Max_Soups'))
 
         elif num == 3:
             num = agi.execute(pystrix.agi.core.WaitForDigit)
+            agi.execute(pystrix.agi.core.ControlStreamFile('Max_Porno_Intro'))
+
             # go into the pre-made porno listings random five.
         else:
             agi.execute(pystrix.agi.core.Hangup())
     elif num == 2:
-        agi.execute(pystrix.agi.core.ControlStreamFile('joe.wav'))
+        intro = random.randint(1, 4)
+        if intro == 1:
+            agi.execute(pystrix.agi.core.ControlStreamFile('Joe_Intro_1'))
+        elif intro == 2:
+            agi.execute(pystrix.agi.core.ControlStreamFile('Joe_Intro_2'))
+        elif intro == 3:
+            agi.execute(pystrix.agi.core.ControlStreamFile('Joe_Intro_3'))
+        else:
+            agi.execute(pystrix.agi.core.ControlStreamFile('Joe_Intro_4'))
+        time.sleep(2)
+        agi.execute(pystrix.agi.core.ControlStreamFile('Joe_Introduction'))
+        time.sleep(2)
+        agi.execute(pystrix.agi.core.ControlStreamFile('Joe_Intro_Response'))
         num = agi.execute(pystrix.agi.core.WaitForDigit)
         if num == 1:
             num = agi.execute(pystrix.agi.core.WaitForDigit)
             # transfer call to conference room
         elif num == 2:
             num = agi.execute(pystrix.agi.core.WaitForDigit)
-            agi.execute(pystrix.agi.core.ControlStreamFile('joe.wav'))
-            if num == 1:
-                agi.execute(pystrix.agi.core.ControlStreamFile('basic.wav'))
-            elif num == 2:
-                agi.execute(pystrix.agi.core.ControlStreamFile('basic+.wav'))
-            elif num == 3:
-                agi.execute(pystrix.agi.core.ControlStreamFile('brutal.wav'))
-            elif num == 4:
-                agi.execute(pystrix.agi.core.ControlStreamFile('brutal+.wav'))
-            elif num == 5:
-                agi.execute(pystrix.agi.core.ControlStreamFile('other.wav'))
-            elif num == 6:
-                agi.execute(pystrix.agi.core.ControlStreamFile('Ultra.wav'))
-            elif num == 7:
-                agi.execute(pystrix.agi.core.ControlStreamFile('Specials.wav'))
-
+            agi.execute(pystrix.agi.core.ControlStreamFile('Joe_Intro_LiveMenu'))
             # Start Live chat info
+            if num == 1:
+                agi.execute(pystrix.agi.core.ControlStreamFile('Joe_Basic'))
+            elif num == 2:
+                agi.execute(pystrix.agi.core.ControlStreamFile('Joe_Basic+'))
+            elif num == 3:
+                agi.execute(pystrix.agi.core.ControlStreamFile('Joe_Brutal'))
+            elif num == 4:
+                agi.execute(pystrix.agi.core.ControlStreamFile('Joe_Brutal+'))
+            elif num == 5:
+                agi.execute(pystrix.agi.core.ControlStreamFile('Joe_Other'))
+            elif num == 6:
+                agi.execute(pystrix.agi.core.ControlStreamFile('Joe_Ultra'))
+            elif num == 7:
+                agi.execute(pystrix.agi.core.ControlStreamFile('Joe_Specials_Intro'))
+                spec = random.randint(1, 6)
+                if spec == 1:
+                    agi.execute(pystrix.agi.core.ControlStreamFile('Joe_UnderTheSea'))
+                elif spec == 2:
+                    agi.execute(pystrix.agi.core.ControlStreamFile('Joe_Lovecraft'))
+                elif spec == 3:
+                    agi.execute(pystrix.agi.core.ControlStreamFile('Joe_Rainbow'))
+                elif spec == 4:
+                    agi.execute(pystrix.agi.core.ControlStreamFile('Joe_Fruit'))
+                elif spec == 5:
+                    agi.execute(pystrix.agi.core.ControlStreamFile('Joe_Bees'))
+                else:
+                    agi.execute(pystrix.agi.core.ControlStreamFile('Joe_Soups'))
+
         elif num == 3:
             num = agi.execute(pystrix.agi.core.WaitForDigit)
-            # go into the pre-made porno listings random five.
+            agi.execute(pystrix.agi.core.ControlStreamFile('Max_Porno_Intro'))
         else:
             agi.execute(pystrix.agi.core.Hangup())
     elif num == 3:
